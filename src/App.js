6cb010c6ecfe2f80b5e99bfe46f1a6f50ec6e1c2
João Henrique Xavier <jhcxavier@gmail.com>
2023-08-04 22:51:40 +0000

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
 // import components 
-import { InputData } from './components/InputDatas';
-import { MyName } from './components/MyName';
 
-export function AppLearnReact() {
 
-  let people = ['João Henrique Xavier', 'Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah'];
-
+export const App=()=> {
+// Add logic here...
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* imported components */}
-        <InputData buttonType="Add" />
-        {
-          // map function (loop through array and output my name for each element)
-          people.map((eachPeople, index) => (
-            <div key={index}>
-              <MyName myName={eachPeople} />
-            </div>
-          ))
-        }
-        <InputData buttonType="Cancel" />
-      </header>
-    </div>
+    <div classNameName="App">
+      Hello
+  </div>
   );
 }
 
--- src/components/MyName.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function MyName(props) {
-    return (
-        <div>
-            <p>{props.myName}</p>
-        </div>
-    )
-}
