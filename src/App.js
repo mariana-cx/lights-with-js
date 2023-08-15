03416e3e279665b2ab0c6e6c83288f69641fb705
mariana-cx <133844552+mariana-cx@users.noreply.github.com>
2023-08-15 18:26:52 -0300

    traffic light

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 // import components 
 
 
 export const App = () => {
   // Add logic here...
+  const RED = "#3b0602"
+  const GREEN = "#113b02"
+  const YELLOW = "#ad8605"
+  const [redLigth, setRedLight] = useState(RED)
+  const [yellowLight, setYellowLight] = useState(YELLOW)
+  const [greenLight, setGreenLight] = useState(GREEN)
+
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setRedLight("red");
+
+      setTimeout(() => {
+        setRedLight(RED);
+        setGreenLight("#3cd406");
+
+        setTimeout(() => {
+          setGreenLight(GREEN);
+          setYellowLight("yellow");
+
+          setTimeout(() => {
+            setYellowLight(YELLOW);
+
+          }, 3000);
+
+        }, 4000);
+
+      }, 4000);
+
+    }, 11000);
+
+  }, [])
+
   return (
     <div className="view">
       <div className='blackCable'></div>
       <div className='lightWrapper'>
-        <div className='light' style={{ background: "#3b0602" }}>
+        <div className='light' style={{ background: redLigth }}>
         </div>
-        <div className='light' style={{ background: "#ad8605" }}>
+        <div className='light' style={{ background: yellowLight }}>
         </div>
-        <div className='light' style={{ background: "#1b5c04" }}>
+        <div className='light' style={{ background: greenLight }}>
         </div>
       </div>
     </div>
