24c63f6c5529bf4212641cbe0d07a5366efd7a5c
mariana-cx <133844552+mariana-cx@users.noreply.github.com>
2023-08-15 16:31:14 -0300

    add light component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 // import components 
 
 
-export const App=()=> {
-// Add logic here...
+export const App = () => {
+  // Add logic here...
   return (
-    <div classNameName="App">
-      Hello
-  </div>
+    <div className="view">
+      <div className='blackCable'></div>
+      <div className='lightWrapper'>
+        <div className='light' style={{ background: "#3b0602" }}>
+        </div>
+        <div className='light' style={{ background: "#ad8605" }}>
+        </div>
+        <div className='light' style={{ background: "#1b5c04" }}>
+        </div>
+      </div>
+    </div>
   );
 }
 
